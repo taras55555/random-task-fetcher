faf1b11e5d24dbed4d434e7579e4d7f133401b73
Тарас Камінський <tkaminskyi@gmail.com>
2024-09-21 17:40:02 +0300

    -optimized MySql query for fetching random tasks - added promise-based error handling - implemented a 404 page handler

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const connection = require('./connection');
 
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT;
 
 app.use(cors());
 
 app.get('/api', (req, res) => {
-    const randomSet = new Set([])
-
-    const sql = 'SELECT task FROM tasks'
-    connection.query(sql, (err, rows) => {
-        if (err instanceof Error) {
-            console.log(err);
-            return;
-        }
-
-        while (randomSet.size !== 10) {
-            const randVal = Math.floor(Math.random() * rows.length);
-            randomSet.add(rows[randVal])
-        }
-
-        res.json(Array.from(randomSet));
+    const randomTasks = new Promise((resolve, reject) => {
+        const sql = 'SELECT task FROM tasks ORDER BY RAND() LIMIT 10'
+        connection.query(sql, (err, rows) => {
+            if (err instanceof Error) {
+                console.log(err);
+                reject({ error: 'Something Went Wrong' });
+            }
+            resolve(rows)
+        })
     })
+
+    randomTasks
+        .then((value) => res.json(value))
+        .catch((err) => res.status(500).json(err))
 })
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+});
 
 app.listen(port || 80, (req, res) => {
     console.log(`Server listening on ${port}`)
